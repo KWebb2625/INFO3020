594d9c2ccadc960a3203f36bfb47b92fb7045490
KWebb2625 <59853206+KWebb2625@users.noreply.github.com>
2020-01-29 23:56:54 -0600

    Add files via upload

---
+++ Months.py
@@ -0,0 +1,8 @@
+# Kathryn Webb
+# 1/29/2020
+# Write a program that uses a for loop to print(January to December)
+# One of the months of the year is January
+# One of the months of the year is February ...
+
+for i in ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"):
+    print("One of the months of the year is", i)
